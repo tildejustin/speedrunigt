e77aa2152b40edf9845459ff87ae76908f9b0fee
tildejustin <spacepepper3.14@gmail.com>
2024-01-08 19:52:36 -0500

    fix translations on quilt and some updates (#97)
    * update fabric loader dependency and target version
    
    * update mappings
    
    * optimize translation loading impl (fixes quilt incompatibility)
    
    * move translation files to their proper location
    # Conflicts:
    #	gradle.properties
    #	src/main/java/com/redlimerl/speedrunigt/mixins/MinecraftClientMixin.java
    #	src/main/java/com/redlimerl/speedrunigt/mixins/translate/TranslationStorageMixin.java
    #	src/main/java/com/redlimerl/speedrunigt/utils/TranslateHelper.java
    #	src/main/resources/fabric.mod.json

--- src/main/java/com/redlimerl/speedrunigt/mixins/translate/TranslationStorageMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/translate/TranslationStorageMixin.java
@@ -2,9 +2,10 @@ package com.redlimerl.speedrunigt.mixins.translate;
 
 import com.redlimerl.speedrunigt.option.SpeedRunOption;
 import com.redlimerl.speedrunigt.option.SpeedRunOptions;
-import com.redlimerl.speedrunigt.utils.TranslateHelper;
+import com.redlimerl.speedrunigt.utils.ResourcesHelper;
 import net.minecraft.client.resource.language.TranslationStorage;
-import net.minecraft.resource.ResourceManager;
+import net.minecraft.resource.Resource;
+import net.minecraft.util.Language;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,31 +13,29 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import java.io.IOException;
 import java.io.InputStream;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Mixin(TranslationStorage.class)
 public abstract class TranslationStorageMixin {
 
     @Shadow protected abstract void load(InputStream inputStream);
 
-    @Inject(method = "load(Lnet/minecraft/resource/ResourceManager;Ljava/util/List;)V", at = @At("RETURN"))
-    private void onLoad(ResourceManager container, List<String> list, CallbackInfo ci) {
-        for (String lang : list) {
-            InputStream inputStream = TranslateHelper.setup(lang, SpeedRunOption.getOption(SpeedRunOptions.ALWAYS_ENGLISH_TRANSLATIONS));
-            if (inputStream == null) continue;
-            this.load(inputStream);
-            try {
-                inputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+    @Inject(method = "load(Ljava/util/List;)V", at = @At("RETURN"))
+    private void onLoad(List<Resource> resources, CallbackInfo ci) {
+        resources.forEach(resource -> {
+            // minecraft always loads en_us as a backup, if using only english translations just skip loading the other attempts
+            if (SpeedRunOption.getOption(SpeedRunOptions.ALWAYS_ENGLISH_TRANSLATIONS) && !resource.getId().getPath().equalsIgnoreCase("lang/en_us.json"))
+                return;
+            Optional.ofNullable(ResourcesHelper.toStream("/assets/speedrunigt/" + resource.getId().getPath())).ifPresent(this::load);
+        });
     }
 
-    @Inject(method = "load(Ljava/util/List;Ljava/util/Map;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/Resource;getInputStream()Ljava/io/InputStream;"), locals = LocalCapture.CAPTURE_FAILSOFT, cancellable = true)
-    private static void cancelExternalLoadingOfTranslations(List<Resource> resources, Map<String, String> translationMap, CallbackInfo ci, Iterator<Resource> resourceIterator, Resource resource) {
+    @Inject(method = "load(Ljava/util/List;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/Resource;getInputStream()Ljava/io/InputStream;"), locals = LocalCapture.CAPTURE_FAILSOFT, cancellable = true)
+    private static void cancelExternalLoadingOfTranslations(List<Resource> resources, CallbackInfo ci, Iterator<Resource> resourceIterator, Resource resource) {
         if (SpeedRunOption.getOption(SpeedRunOptions.ALWAYS_ENGLISH_TRANSLATIONS) && resource.getId().getNamespace().equals("speedrunigt") && !resource.getId().getPath().equalsIgnoreCase("lang/en_us.json")) {
             ci.cancel();
         }
