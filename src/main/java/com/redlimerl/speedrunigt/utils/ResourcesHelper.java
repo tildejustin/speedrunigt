c250cb6cd0931b2a787d4eed4d636dad855af56c
tildejustin <spacepepper3.14@gmail.com>
2025-01-02 09:50:18 -0500

    fix quilt compat part 2 (#215)

--- src/main/java/com/redlimerl/speedrunigt/utils/ResourcesHelper.java
+++ src/main/java/com/redlimerl/speedrunigt/utils/ResourcesHelper.java
@@ -1,46 +1,33 @@
 package com.redlimerl.speedrunigt.utils;
 
-import com.google.common.collect.Lists;
-
-import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
+import java.nio.file.*;
+import java.util.stream.Stream;
 
 public class ResourcesHelper {
-    public static String[] getResourceChildren(String path) throws IOException, URISyntaxException {
-        File jarFile = new File(ResourcesHelper.class.getProtectionDomain().getCodeSource().getLocation().toURI());
-        ArrayList<String> list = Lists.newArrayList();
-
-        if (jarFile.isFile()) {
-            ZipInputStream zip = new ZipInputStream(ResourcesHelper.class.getProtectionDomain().getCodeSource().getLocation().openStream());
-            while (true) {
-                ZipEntry e = zip.getNextEntry();
-                if (e == null) { break; }
-                String name = e.getName();
-                if (name.startsWith(path)) {
-                    String[] fileName = name.split("/");
-                    String file = fileName[fileName.length - 1];
-                    if (!file.isEmpty() && fileName.length > 1) {
-                        list.add("/" + path + "/" + file);
-                    }
-                }
-            }
-        } else {
-            URL url = ResourcesHelper.class.getResource("/" + path);
-            if (url != null) {
-                File apps = new File(url.toURI());
-                for (File app : Objects.requireNonNull(apps.listFiles())) {
-                    list.add("/" + path + "/" + app.getName());
-                }
-            }
+    public static String[] getResourceChildren(String folder) throws IOException, URISyntaxException {
+        URL parent = ResourcesHelper.class.getResource("/" + folder);
+        if (parent == null) {
+            throw new FileNotFoundException(folder);
         }
-        return list.toArray(new String[0]);
+        Path path;
+        if (parent.getProtocol().equals("jar")) {
+            // someone already opened the jar, probably fabric loader
+            FileSystem fs = FileSystems.getFileSystem(parent.toURI());
+            if (fs == null) {
+                throw new FileSystemNotFoundException(parent.toURI().toString());
+            }
+            path = fs.getPath("/" + folder);
+        } else {
+            path = Paths.get(parent.toURI());
+        }
+        try (Stream<Path> children = Files.list(path)) {
+            return children.map(Path::toString).toArray(String[]::new);
+        }
     }
 
     public static InputStream toStream(String resource) {
