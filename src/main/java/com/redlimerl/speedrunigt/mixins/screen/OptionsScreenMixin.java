24206863afa372fdcd1dfd7e4195a8a9037a2d8a
tildejustin <spacepepper3.14@gmail.com>
2025-07-27 19:46:50 -0400

    fix backport

--- src/main/java/com/redlimerl/speedrunigt/mixins/screen/OptionsScreenMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/screen/OptionsScreenMixin.java
@@ -30,7 +30,7 @@ public class OptionsScreenMixin extends Screen {
     @Inject(method = "init", at = @At("TAIL"))
     private void onInit(CallbackInfo ci) {
         if (hasSpeedrunAPI) return;
-        this.timerButton = new ButtonWidget(this.width / 2 - 180, this.height / 6 - 12, 20, 20, new LiteralText(""), (buttonWidget) -> {
+        this.timerButton = new ButtonWidget(this.width / 2 - 180, this.height / 6 - 12, 20, 20, "", (buttonWidget) -> {
             if (this.client != null) {
                 this.client.openScreen(new SpeedRunOptionScreen(this));
             }
